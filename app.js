7edcb1983cfeeec8fd4498469d8daaab173c297b
Dave Spina <dspina79@gmail.com>
2022-09-05 11:09:32 -0400

    Added second animation

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 let animation = anime({
-    targets: 'div',
+    targets: '#blocker',
     translateX: 100,
-    borderRadius: 20,
+    translateY: 100,
+    borderRadius: 30,
     duration: 2000,
     easing: 'linear',
-    direction: 'alternate'
+    direction: 'alternate',
+    loop: true
 });
+
+let amation2 = anime({
+    targets: '#blocker2',
+    translateX: 0,
+    translateY: 300,
+    duration: 4000,
+    borderRadius: 100,
+    easing: 'linear',
+    direction: 'alternate'
+})
